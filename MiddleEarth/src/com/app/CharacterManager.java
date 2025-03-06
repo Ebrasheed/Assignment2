520b997b5f23f926fd8f03ae2cde41af7cb3e3aa
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-03-05 23:29:42 -0600

    Update CharacterManager.java
    added addCharacter method that adds a character and doubles array size if full

--- MiddleEarth/src/com/app/CharacterManager.java
+++ MiddleEarth/src/com/app/CharacterManager.java
@@ -8,3 +8,17 @@ class CharacterManager {
         characters = new MiddleEarthCharacter[5];
         size = 0;
     }
+/**
+     * this method adds a character and doubles the array size if full
+     * @param c
+     * character to be added
+     * @return
+     * return true if character was added
+     */
+    public boolean addCharacter(MiddleEarthCharacter c) {
+        if (size >= characters.length) {
+            resizeArray();
+        }
+        characters[size++] = c;
+        return true;
+    }
