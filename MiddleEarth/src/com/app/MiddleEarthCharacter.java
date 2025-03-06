589c5362ca23b0634c590bafee527a7adf028c73
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-03-05 23:03:02 -0600

    Update MiddleEarthCharacter.java

--- MiddleEarth/src/com/app/MiddleEarthCharacter.java
+++ MiddleEarth/src/com/app/MiddleEarthCharacter.java
@@ -1,21 +1,21 @@
 package com.app;
 
 abstract class MiddleEarthCharacter {
-	 	protected String name;
-	    protected double health;
-	    protected double power;
+	protected String name;
+	protected double health;
+	protected double power;
 
-	    public MiddleEarthCharacter(String name, double health, double power) {
-	        this.name = name;
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
 	        this.health = health;
 	        this.power = power;
 	    }
 
-	    public abstract boolean attack(MiddleEarthCharacter target);
-	    public abstract String getRace();
+	public abstract boolean attack(MiddleEarthCharacter target);
+	public abstract String getRace();
 
-	    public void displayInfo() {
-	        System.out.println("Name: " + name + ", Race: " + getRace() + ", Health: " + health + ", Power: " + power);
+	public void displayInfo() {
+		System.out.println("Name: " + name + ", Race: " + getRace() + ", Health: " + health + ", Power: " + power);
 	    }
 }
 
