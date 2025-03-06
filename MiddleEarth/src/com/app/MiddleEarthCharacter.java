5ceed7942c0361204d0cd05ab4be37fae55deb1b
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-03-05 23:02:03 -0600

    Update MiddleEarthCharacter.java
    added an abstract class for the subclasses to extend from

--- MiddleEarth/src/com/app/MiddleEarthCharacter.java
+++ MiddleEarth/src/com/app/MiddleEarthCharacter.java
@@ -1,1 +1,21 @@
+package com.app;
+
+abstract class MiddleEarthCharacter {
+	 	protected String name;
+	    protected double health;
+	    protected double power;
+
+	    public MiddleEarthCharacter(String name, double health, double power) {
+	        this.name = name;
+	        this.health = health;
+	        this.power = power;
+	    }
+
+	    public abstract boolean attack(MiddleEarthCharacter target);
+	    public abstract String getRace();
+
+	    public void displayInfo() {
+	        System.out.println("Name: " + name + ", Race: " + getRace() + ", Health: " + health + ", Power: " + power);
+	    }
+}
 
