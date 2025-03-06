c86223801824f58967820a66afc4c31fc975b19d
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-03-05 23:08:16 -0600

    Update Elf.java
    Created elf class that extends the MiddleEarthCharacter class and overrides the attack and getRace methods

--- MiddleEarth/src/com/app/Elf.java
+++ MiddleEarth/src/com/app/Elf.java
@@ -1,1 +1,25 @@
+package com.app;
 
+class Elf extends MiddleEarthCharacter {
+    public Elf(String name, double health, double power) {
+        super(name, health, power);
+    }
+
+    @Override
+    public boolean attack(MiddleEarthCharacter target) {
+        if (target instanceof Orc) {
+            target.health -= this.power * 1.5;
+            return true;
+        } else if (target instanceof Dwarf || target instanceof Elf) {
+            return false;
+        } else {
+            target.health -= this.power;
+            return true;
+        }
+    }
+
+    @Override
+    public String getRace() {
+        return "Elf";
+    }
+}
